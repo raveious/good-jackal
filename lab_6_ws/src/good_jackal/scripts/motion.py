00a5bd185c7d429e4a84b3dba835c02d94b5fa45
Ian Wakely <raveious.irw@gmail.com>
2017-03-20 16:00:12 -0400

    Adding working turning 'tuned' code

--- lab_6_ws/src/good_jackal/scripts/motion.py
+++ lab_6_ws/src/good_jackal/scripts/motion.py
@@ -5,70 +5,88 @@
 # By: Ian Wakely
 
 import rospy
+import numpy
 from time import time
 from geometry_msgs.msg import Twist
 from good_jackal.msg import Tracked_Object
 
 class BallFollower(object):
     turn_p_gain = 0.003
-    turn_i_gain = 0.0
+    turn_i_gain = 0.02
+    turn_d_gain = 0.0
 
     speed_p_gain = 0.0
     speed_i_gain = 0.0
 
-    def __init__(self, radius = 18):
+    def __init__(self, distance, timeout = 500):
         # init node
         rospy.init_node("motion", anonymous=False)
         self.object_sub = rospy.Subscriber("/good_jackal/object", Tracked_Object, self._ballLocation)
         self.motion_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
         self.prev_t_i_val = 0
-        self.prev_d_i_val = 0
+        self.prev_s_i_val = 0
         self.prev_time = time()
 
-        self.target_radius = radius
+        self.target_distance = distance
 
-        # standard node spin
-        rospy.spin()
+        rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown():
+            if time() - self.prev_time > timeout:
+                rospy.logwarn("No recent image?")
+                self.motion_pub.publish(Twist())
+                self.prev_t_i_val = 0
+                self.prev_s_i_val = 0
+
+            rate.sleep()
+
+    def _findRange(self, pixels):
+        return numpy.interp(pixels,
+                [27, 30, 34, 39, 48, 62, 89],
+                [2.4384, 2.1336, 1.8288, 1.524, 1.2192, 0.9144, 0.6096])
 
     def _ballLocation(self, data):
         curr_time = time()
-        rospy.loginfo(data)
-
-        # clear stale I values
-        # if curr_time - self.prev_time > 3000:
-        #     self.prev_i_val = 0
+        deltat = curr_time - self.prev_time
+        rospy.loginfo("Delta T: {}".format(deltat))
 
         t_p = -data.x * self.turn_p_gain
-        t_i = (-data.x - self.prev_t_i_val) * (curr_time - self.prev_time) * self.turn_i_gain
-        turning = t_p + t_i
+        t_i = (-data.x - self.prev_t_i_val) * deltat * self.turn_i_gain
+
+        # Make a dead zone...
+        if abs(data.x) < 3:
+            turning = 0
+        else:
+            turning = t_p + t_i
+
+        rospy.loginfo("Turning: {}".format(turning))
 
         # save latest value for next I eval
         self.prev_t_i_val = -data.x
+        self.prev_time = curr_time
 
+        distance = self._findRange(data.r)
+        distance_err = distance - self.target_distance
+        rospy.loginfo("Distance: {} (Err: {}, R: {})".format(distance, distance_err, data.r))
 
-        distance = self.target_radius - data.r
-
-
-        s_p = data.r * self.speed_p_gain
+        s_p = distance_err * self.speed_p_gain
         s_i = 0
 
         speed = s_p + s_i
 
-        rospy.loginfo("Turning: {}".format(turning))
-        rospy.loginfo("Distance: {}".format(distance))
         rospy.loginfo("Speed: {}".format(speed))
 
         twist = Twist()
         twist.linear.x = 0
         twist.angular.z = turning
 
-        rospy.logdebug("Twist: ", twist)
+        rospy.logdebug(twist)
         self.motion_pub.publish(twist)
 
 # standard ros boilerplate
 if __name__ == "__main__":
     try:
-        motion = BallFollower()
+        motion = BallFollower(1.5, 500)
     except rospy.ROSInterruptException:
         pass
